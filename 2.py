e77c6bc292830cfb1fae65ade124d00aba30ef02
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-19 16:31:02 +0300

    Update 2.py
    ыыыы

--- 2.py
+++ 2.py
@@ -1,25 +1,15 @@
 import numpy as np
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_function_seaborn(func, x_range, step=0.01):
-    x = np.arange(x_range[0], x_range[1] + step, step)
-    y = func(x)
-    # DataFrame для использования с seaborn
-    df = pd.DataFrame({'x': x, 'y': y})
+def plot_function():
+    x = np.arange(0, 4.01, 0.01)  # Создаем массив x с шагом 0.01
+    y = np.cos(20 * x) / (x + 0.1) # Вычисляем значения y
 
-    sns.set(style="whitegrid")
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x='x', y='y', data=df, color='blue', linewidth=2.0)
-    plt.xlabel("x", fontsize=12)
-    plt.ylabel("y(x)", fontsize=12)
-    plt.title("График функции", fontsize=16)
-    plt.grid(True)  # Отображение сетки
+    plt.plot(x, y)
+    plt.xlabel("x")
+    plt.ylabel("y(x)")
+    plt.title("График функции y(x) = cos(20x) / (x + 0.1)")
+    plt.grid(True)
     plt.show()
-def my_function(x):
-    return np.cos(20 * x) / (x + 0.1)
-x_range = (0, 4) # Диапазон x и шаг котоыре у нас так то в домашке
-step = 0.01
 
-plot_function_seaborn(my_function, x_range, step)
+plot_function()
